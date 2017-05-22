076fb9855f14972ec4ef3b228d86a18e38f0bdb8
makslevental <maksim.levental@gmail.com>
2017-05-22 18:29:43 -0400

    update eslint in package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {Provider} from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Router} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./components/App";
 import "./scss/index.css";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
