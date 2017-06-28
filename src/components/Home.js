0bb5b115173ea1cf4c8e717b2a8945c67dafa866
Minyong Cho <minyongcho@128-122-236-241.DYNAPOOL.NYU.EDU>
2017-06-28 15:01:54 -0400

    change tab title to welcome databrary

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
         return (
             <div className="home">
                 <div className="home-header">
-                    <img src={`${config.static}/images/logo.svg`} className="home-logo" alt="logo"/>
+                    <object data={`${config.static}/images/logo.svg`} className="home-logo" alt="logo"></object>
                     <h2>Welcome to Databrary</h2>
                 </div>
                 <p className="home-intro">
